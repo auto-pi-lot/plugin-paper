b4eed28455632aef0ba21c2248e0ae7e53363738
sneakers-the-rat <JLSaunders987@gmail.com>
2022-05-05 17:32:09 -0700

    testing sound

---
+++ plugin_tests/scripts/test_sound.py
@@ -0,0 +1,64 @@
+"""
+Test sound latency
+"""
+
+from autopilot import external
+from autopilot.stim.sound import jackclient
+from autopilot.stim.sound import sounds
+from autopilot.hardware.gpio import Digital_In, Digital_Out
+from autopilot import prefs
+import time
+import argparse
+
+def start_jack_server():
+    jackd_process = external.start_jackd()
+    server = jackclient.JackClient()
+    return jackd_process, server
+
+def test_sound(n_reps:int=-1, iti=0.5, duration:float=100):
+    tone = sounds.Tone(10000, duration=100, amplitude=0.01)
+    tone.buffer()
+
+    out_conf = prefs.get('HARDWARE')['GPIO']['digi_out']
+    in_conf = prefs.get('HARDWARE')['GPIO']['digi_in']
+    pin_out = Digital_Out(**out_conf)
+    pin_in = Digital_In(**in_conf)
+
+    pin_in.assign_cb(tone.play)
+
+    n_loops = 0
+    try:
+        while n_reps < 0 or n_loops < n_reps:
+
+            pin_out.pulse()
+            tone.stop_evt.wait()
+            tone.buffer()
+
+            time.sleep(iti)
+            n_loops += 1
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        pin_out.release()
+        pin_in.release()
+
+
+def make_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        "Test Sound latency")
+    parser.add_argument(
+        '-n','--n_reps', help="Number of times to run each test",
+        type=int, default=500, required=False)
+    parser.add_argument(
+        '-i', '--iti', help="Number of ms to wait in between each test",
+        type=float, default=0.5, required=False
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    parser = make_parser()
+    args = parser.parse_args()
+
+    test_sound(args.n_reps, args.iti)
